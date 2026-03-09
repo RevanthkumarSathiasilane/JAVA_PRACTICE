44f09a2bdb1799bfa9524b7cf4922bedc8f41fc9
Revanthkumar <rkrevanth2908@gmail.com>
2026-03-09 20:42:41 +0530

    First code of OOP

---
+++ src/com/oops/Main.java
@@ -0,0 +1,11 @@
+package com.oops;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        //store 5 roll nos:
+        int[] rno = new int[5];
+        //store 5 names:
+        String[] name = new String[5];
+
+    }
+}
