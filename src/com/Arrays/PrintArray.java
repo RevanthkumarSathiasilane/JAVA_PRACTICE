035d14d3491097dd430c463cc4d355aba7bed5a2
Revanthkumar <rkrevanth2908@gmail.com>
2026-03-10 15:36:54 +0530

    Arrays package created

---
+++ src/com/Arrays/PrintArray.java
@@ -0,0 +1,8 @@
+package com.Arrays;
+import java.util.*;
+public class PrintArray {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println()
+    }
+}
