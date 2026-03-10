b7a815b2968dd4046aed2ebff6285d0845cd0870
Revanthkumar <rkrevanth2908@gmail.com>
2026-03-10 15:41:39 +0530

    PrintArray Done

--- src/com/Arrays/PrintArray.java
+++ src/com/Arrays/PrintArray.java
@@ -3,6 +3,12 @@ import java.util.*;
 public class PrintArray {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-       System.out.println()
+       System.out.print("Enter array size:");
+       int size = sc.nextInt();
+       int[] arr = new int[size];
+       for(int row=0;row<arr.length;row++){
+           arr[row] = sc.nextInt();
+       }
+       System.out.println(Arrays.toString(arr));
     }
 }
