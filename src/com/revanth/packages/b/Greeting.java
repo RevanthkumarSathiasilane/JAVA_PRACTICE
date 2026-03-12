b5915cdd736115e0ad3769946745ef9fea60cdf7
Revanthkumar <rkrevanth2908@gmail.com>
2026-03-12 21:46:13 +0530

    OOP2 concepts half updated

---
+++ src/com/revanth/packages/b/Greeting.java
@@ -0,0 +1,10 @@
+package com.revanth.packages.b;
+
+import static com.revanth.packages.b.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("I am awesome");
+        message();
+    }
+}
---
+++ src/com/revanth/staticExample/Main.java
@@ -0,0 +1,24 @@
+package com.revanth.staticExample;
+
+public class Main {
+    public static void main(String[] args) {
+//        Human revanth = new Human(29,"revan",50_000, false);
+//        Human rahul = new Human(22,"rahul",60_000, true);
+//        Human praveen = new Human(28,"praveen",1_00_000, false);
+//
+//        System.out.println(Human.population);
+//        System.out.println(Human.population);
+//        System.out.println(Human.population);
+
+
+    }
+    static void fun(){
+        greeting();// you cant use this, because it requires an instance
+        //but the function you are using it in does not depend on instances.
+
+    }
+    //we know that something which is not static, belongs to an object
+     void greeting(){
+        System.out.println("Hello World!!");
+    }
+}
