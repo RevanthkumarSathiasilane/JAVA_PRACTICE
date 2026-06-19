609da37aa7ccbea387f309f4eb4560674717e0c4
Revanthkumar <rkrevanth2908@gmail.com>
2026-06-19 15:53:05 +0530

    Product of Digits is completed

---
+++ src/com/Recursion/ProdofDigits.java
@@ -0,0 +1,11 @@
+package com.Recursion;
+
+public class ProdofDigits {
+    public static void main(String[] args) {
+        System.out.println(prod(1234));
+    }
+    static int prod(int n){
+        if(n%10 == n) return n;
+        return (n%10)* prod(n/10);
+    }
+}
