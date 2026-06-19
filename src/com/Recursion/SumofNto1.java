3470f42c1df84df2e006e554646632f9b9b1ad52
Revanthkumar <rkrevanth2908@gmail.com>
2026-06-19 15:11:09 +0530

    Sum of n to 1 finished using recursion

---
+++ src/com/Recursion/SumofNto1.java
@@ -0,0 +1,12 @@
+package com.Recursion;
+
+public class SumofNto1 {
+    public static void main(String[] args) {
+       int n = 5;
+        System.out.println(add(n));
+    }
+    static int add(int n){
+        if(n==1) return 1;
+        return n+add(n-1);
+    }
+}
